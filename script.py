02ab20a58bfe4b4a33fc5bcc4d8bc7e355b1cbf6
Avi Sharma <64670742+Av1Sharma@users.noreply.github.com>
2026-06-28 14:37:08 -0400

    script try

--- script.py
+++ script.py
@@ -1,3 +1,25 @@
 import os ## need computer access
 import face_recognition ## see step 1
 import cv2 ## also step 1
+
+## Since my folder is nearly 50Gbs big, I will not create a folder called unsorted photos. I will merely point the script to the folder
+
+TARGET_FOLDER = "/Users/avi/Desktop/RoboticsPhotos"
+
+VALID_EXTENSIONS = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff"]
+
+for filename in os.listdir(TARGET_FOLDER): ## Simple for loop, looping through each file in directory
+    if not filename.lower().endswith(VALID_EXTENSIONS): ## checking if the file is an image
+        continue ## skipping if not an image
+    
+    file_path = os.path.join(TARGET_FOLDER, filename)
+    print(f"Processing {filename}...")
+
+    try:
+        image = face_recognition.load_image_file(file_path) ## loads the image.
+        
+        face_locations = face_recognition.face_locations(image, model="hog")
+        print(f"-> Found {len(face_locations)} face(s) in this photo.")
+        
+    except Exception as e:
+        print(f"-> Could not process {filename}. Error: {e}")
